2ad37d58f358758cfd3ee121c4f43e8019746286
kamal11 <kyzx11@gmail.com>
2018-10-22 23:50:58 +0530

    finished javascript code

--- shared.js
+++ shared.js
@@ -1,12 +1,41 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlasButtons = document.querySelectorAll(".plan button");
-
-selectPlasButtons.map;
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlasButtons.length; i++) {
   selectPlasButtons[i].addEventListener("click", () => {
-    modal.style.display = "block";
-    modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // modal.className = "open"
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
+
+backdrop.addEventListener("click", () => {
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  if (modal) modal.classList.remove("open");
+
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", () => {
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
