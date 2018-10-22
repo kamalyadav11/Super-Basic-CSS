57f344cbb40102e19c85ae7828423b089c98ce23
kamal11 <kyzx11@gmail.com>
2018-10-22 23:05:39 +0530

    added js file

---
+++ shared.js
@@ -0,0 +1,12 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlasButtons = document.querySelectorAll(".plan button");
+
+selectPlasButtons.map;
+
+for (var i = 0; i < selectPlasButtons.length; i++) {
+  selectPlasButtons[i].addEventListener("click", () => {
+    modal.style.display = "block";
+    modal.style.display = "block";
+  });
+}
